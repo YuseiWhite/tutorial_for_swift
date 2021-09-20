91ff661ba80a319952355c951a57ef0ed207f318
yuseirf <yusei.tanaka2112325@icloud.com>
2021-09-20 19:55:54 +0900

    Initial Commit

---
+++ LandMarks/LandMarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandMarksApp.swift
+//  LandMarks
+//
+//  Created by 田中優成 on 2021/09/20.
+//
+
+import SwiftUI
+
+@main
+struct LandMarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
